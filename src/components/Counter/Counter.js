d2d24930a2ea676c84c55f5a90c723f331aa3498
Mary Francis <mary.francis@creditas.com.br>
2019-06-27 14:38:34 -0300

    Simple counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      clickCount: 0
+    }
+  }
+
+  addCount = () => {
+    this.setState({
+      clickCount: this.state.clickCount + 1 
+    })
+
+    console.log('click')
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Counter!</h1>
+        <div>Clicked {this.state.clickCount} times~</div>
+        <button onClick={() => this.addCount()}>Click me!</button>
+      </>
+    )
+  }
+}
+
+export { Counter }
