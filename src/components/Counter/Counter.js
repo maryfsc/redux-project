a5a4ecba3e8e808bb31029f4da5a52d69ef892ef
Mary Francis <mary.francis@creditas.com.br>
2019-06-28 14:58:11 -0300

    changes props name values to clicks for better understanding

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { addCounterValue, decrementCounterValue } from '../../actions/actions'
 import { connect } from 'react-redux'
 
-const Counter = ({ increment, decrement, value }) => {
+const Counter = ({ increment, decrement, clicks }) => {
   return (
     <>
       <h1>Counter!</h1>
-      <div>Clicked {value} times~</div>
+      <div>Clicked {clicks} times~</div>
       <button onClick={increment}>Click me!</button>
       <button onClick={decrement}>Click me too!</button>
     </>
@@ -21,7 +21,7 @@ const mapDispatchToProps = dispatch => {
 }
 
 const mapStateToProps = state => ({
-  value: state.value
+  clicks: state.value
 })
 
 const enhanceCounter = connect(
